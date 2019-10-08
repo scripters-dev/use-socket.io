191b0ed2537d9760403b16feddadcc872b7f3b44
wfalowski <falowski.w@gmail.com>
2019-10-08 19:20:19 +0200

    feat: export useListener and useEmit hooks

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import Provider from './provider';
 import useSocket from './useSocket';
+import useListener from './useListener';
+import useEmit from './useEmit';
 
 export {
     Provider,
     useSocket,
+    useListener,
+    useEmit,
 };
