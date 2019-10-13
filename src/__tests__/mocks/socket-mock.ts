197be16510716935f888903267c00a34cddd3c45
wfalowski <falowski.w@gmail.com>
2019-10-13 13:43:08 +0200

    test: adjust hasListener function testing

--- src/__tests__/mocks/socket-mock.ts
+++ src/__tests__/mocks/socket-mock.ts
@@ -1,12 +1,19 @@
 let connected = true;
+let listeners: Array<string> = [];
 
 export default ({
     id: 'test',
     connected,
     open: jest.fn(),
     emit: jest.fn(),
-    on: jest.fn(),
-    removeListener: jest.fn(),
+    on: jest.fn((eventName) => listeners.push(eventName)),
+    removeListener: jest.fn((eventName) => {
+        listeners = listeners.filter((event) => event !== eventName);
+    }),
     disconnect: () => { connected = false; },
-    hasListeners: (value: string) => value !== 'test',
+    hasListeners: (eventName: string) => listeners.includes(eventName),
 });
+
+export const cleanupListeners = () => {
+    listeners = [];
+};
--- src/__tests__/useListener.test.tsx
+++ src/__tests__/useListener.test.tsx
@@ -3,7 +3,7 @@ import { renderHook } from '@testing-library/react-hooks';
 
 import Provider from '../provider';
 import useListener from '../useListener';
-import mockSocket from './mocks/socket-mock';
+import mockSocket, { cleanupListeners } from './mocks/socket-mock';
 
 const url = 'http://local.test/';
 
@@ -13,6 +13,7 @@ describe('Test useSocket', () => {
     beforeEach(() => {
         mockSocket.on.mockClear();
         mockSocket.removeListener.mockClear();
+        cleanupListeners();
     });
 
     it('should be automatically called', () => {
@@ -64,10 +65,49 @@ describe('Test useSocket', () => {
         );
 
         expect(result.current).toBeInstanceOf(Array);
+        result.current[0]();
         result.current[1]();
 
-        expect(mockSocket.on).toBeCalledTimes(0);
+        expect(mockSocket.on).toBeCalledTimes(1);
         expect(mockSocket.removeListener).toBeCalledTimes(1);
+        expect(result.current[0]).toBeInstanceOf(Function);
+        expect(result.current[1]).toBeInstanceOf(Function);
+    });
+
+    it('should be subscribed only once', () => {
+        const wrapper = ({ children }: any) => (<Provider url={url}>{children}</Provider>);
+        const { result } = renderHook(
+            () => useListener('test-failed', () => {}, { autoSubscribe: false }),
+            { wrapper },
+        );
+
+        expect(result.current).toBeInstanceOf(Array);
+        result.current[0]();
+        result.current[0]();
+        result.current[0]();
+
+        expect(mockSocket.on).toBeCalledTimes(1);
+        expect(mockSocket.removeListener).toBeCalledTimes(0);
+        expect(result.current[0]).toBeInstanceOf(Function);
+        expect(result.current[1]).toBeInstanceOf(Function);
+    });
+
+    it('should be subscribed and unsubscribed few times', () => {
+        const wrapper = ({ children }: any) => (<Provider url={url}>{children}</Provider>);
+        const { result } = renderHook(
+            () => useListener('test-failed', () => {}),
+            { wrapper },
+        );
+
+        expect(result.current).toBeInstanceOf(Array);
+        result.current[0]();
+        result.current[1]();
+        result.current[0]();
+        result.current[1]();
+        result.current[0]();
+
+        expect(mockSocket.on).toBeCalledTimes(3);
+        expect(mockSocket.removeListener).toBeCalledTimes(2);
         expect(result.current[0]).toBeInstanceOf(Function);
         expect(result.current[1]).toBeInstanceOf(Function);
     });
