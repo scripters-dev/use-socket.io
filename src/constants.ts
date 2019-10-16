26d72cfea9602eaae576afbac3b87d5907bce7bf
Wojciech Fałowski <falowski.w@gmail.com>
2019-10-16 07:58:27 +0200

    feat: provide constants with built-in events (#11)

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+// eslint-disable-next-line
+export const EVENTS = {
+    CONNECT: 'connect',
+    CONNECT_ERROR: 'connect_error',
+    CONNECT_TIMEOUT: 'connect_timeout',
+    ERROR: 'error',
+    DISCONNECT: 'disconnect',
+    RECONNECT: 'reconnect',
+    RECONNECT_ATTEMPT: 'reconnect_attempt',
+    RECONNECT_ERROR: 'reconnect_error',
+    RECONNECT_FAILED: 'reconnect_failed',
+    PING: 'ping',
+    PONG: 'pong',
+};
--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import Provider from './provider';
 import useSocket from './useSocket';
 import useListener from './useListener';
 import useEmit from './useEmit';
+import { EVENTS } from './constants';
 
 export {
     Provider,
     useSocket,
     useListener,
     useEmit,
+    EVENTS,
 };
